80ff199a08e82b960d6c2bc1f865a5de32348d0f
jbs <autoj@naver.com>
2017-09-16 18:28:08 +1000

    changed in the test4

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 this is the original file added in the test4
 add in the master
-add in the master but origianl changed also changed in the test4
+add in the master but origianl changed also changed in the test422
 mod and add in the master
 add in the tesing4
